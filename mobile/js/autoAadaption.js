2234a144204a250471a9048a06e0a4fa8602381e
color1004 <1083237997@qq.com>
2018-07-10 10:25:41 +0800

    Update autoAadaption.js

--- mobile/js/autoAadaption.js
+++ mobile/js/autoAadaption.js
@@ -6,6 +6,7 @@
         if (clientWidth >= 750) {
             clientWidth = 750;
         }
+        console.log(window.devicePixelRatio); // 物理分辨率 / 设备分辨率  iPhone 2 Plus 3  电脑 1
         document.documentElement.style.fontSize = 100 * (clientWidth / 750) + 'px';
     };
 window.addEventListener(resizeEvt, recalc, false);
